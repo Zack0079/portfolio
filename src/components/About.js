2673316eb720fbc54f2c5fdc7731fb8066f75817
Zack0079 <zackmoonca@gmail.com>
2025-03-26 05:46:58 -0400

    replace ' to &apos;

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
         <div className="container mx-auto text-center">
           <h2 className="text-3xl font-bold mb-8">About Me</h2>
           <p className="text-lg mb-4 lg:w-lg md:w-md mx-auto text-start">
-          I'm a passionate developer who creates various applications and software using diverse technologies. I love learning new skills and exploring emerging technologies.<br/>
+          I&apos;m a passionate developer who creates various applications and software using diverse technologies. I love learning new skills and exploring emerging technologies.<br/>
           <br/>
           In my free time, I enjoy playing video games, watching anime, and playing board games. I also like watching sports matches, especially NBA, F1, and football.
           </p>
