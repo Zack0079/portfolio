8cb8b28011adba8f98946e1ad32ba681fe59ede5
Zack0079 <zackmoonca@gmail.com>
2025-10-04 06:46:42 -0400

    Modifed the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,11 @@
 export default function Footer() {
-    return (
-      <footer className="bg-gray-800 text-white py-4 text-center">
-        <p>&copy; {new Date().getFullYear()} My Portfolio. All rights reserved.</p>
-      </footer>
-    );
-  }
+  return (
+    <footer className="bg-gray-800 text-white py-4 text-center">
+      <div className="container mx-auto flex flex-row gap-4 justify-between">
+        <span className="">&copy; {new Date().getFullYear()} Cheung Yuet Tse (Zack Tse). All rights reserved.</span >
+        <span className="">Built with Next.js, FontAwesome, and Tailwind CSS</span>
+      </div>
+
+    </footer>
+  );
+}
