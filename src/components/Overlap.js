bb2370da6e5f87332a8691166aa581bd519ae117
Zack0079 <zackmoonca@gmail.com>
2026-09-24 17:26:28 -0400

    Update Overlap.js

--- src/components/Overlap.js
+++ src/components/Overlap.js
@@ -30,16 +30,15 @@ export default function Overlap() {
     return (
         <section id="overlap" style={{ display: activeDiv ? 'block' : 'none' }} className="text-white absolute w-full h-full z-10" >
             <StarsCanvas count={0} />
-            <div className="w-full h-full flex flex-col gap-4 text-center items-center justify-center">
-                <h1 className="abril-fatface-regular text-4xl pb-5">Hi! How are you today?</h1>
-                <h1 className="abril-fatface-regular text-4xl">Welcome to my portfolio!</h1>
+            <div className="w-full h-full flex flex-col gap-4 text-center items-center justify-center px-4">
+                <h1 className="abril-fatface-regular text-2xl sm:text-3xl md:text-4xl pb-5">Hi! How are you today?</h1>
+                <h1 className="abril-fatface-regular text-2xl sm:text-3xl md:text-4xl">Welcome to my portfolio!</h1>
 
-
-                <button className="outline-4 mt-10 rounded-full px-3 py-2 text-3xl font-bold hover:text-gray-400 hover:italic" onClick={() => setActiveDiv(false)}>
+                <button className="outline-4 mt-10 rounded-full px-3 py-2 text-xl sm:text-2xl md:text-3xl font-bold hover:text-gray-400 hover:italic" onClick={() => setActiveDiv(false)}>
                     Enter
                 </button>
             </div>
-            <button className="absolute top-5 right-5 text-3xl hover:text-gray-400" onClick={() => setActiveDiv(false)}>
+            <button className="absolute top-5 right-5 text-2xl sm:text-3xl hover:text-gray-400" onClick={() => setActiveDiv(false)}>
                 <FontAwesomeIcon icon={faCircleXmark} />
             </button>
         </section>
