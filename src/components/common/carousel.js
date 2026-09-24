fe92dc54b68f170e71d76e30a8c8b6e23c7baee0
Zack0079 <zackmoonca@gmail.com>
2026-09-24 17:33:39 -0400

    Modified the carousel to be Peek Style and hidden araow in mobile size

--- src/components/common/carousel.js
+++ src/components/common/carousel.js
@@ -1,6 +1,7 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "../../app/projects.css";
 
 export default function MultiCardCarousel({ projects }) {
     const settings = {
@@ -9,17 +10,31 @@ export default function MultiCardCarousel({ projects }) {
         slidesToShow: 1,
         slidesToScroll: 1,
         infinite: true,
+        swipe: true,
+        swipeToSlide: true,
+        touchThreshold: 10,
+        adaptiveHeight: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    centerMode: false,
+                    centerPadding: "10%",
+                    slidesToShow: 1,
+                },
+            },
+        ],
     };
 
     return (
-        <div>
+        <div className="pb-8 md:pb-0">
             <Slider {...settings}>
                 {projects.map((project, index) => (
-                    <div key={index} className="p-4">
-                        <div className="bg-gray-800 p-6 rounded-lg shadow-lg h-full flex flex-col">
-                            <h3 className="text-xl font-bold mb-2 text-white">{project.title}</h3>
-                            <p className="text-gray-300 mb-4 flex-grow">{project.description}</p>
-                            <div className="flex-row flex justify-between">
+                    <div key={index} className="p-2 sm:p-4">
+                        <div className="bg-gray-800 p-4 sm:p-6 rounded-lg shadow-lg h-full flex flex-col">
+                            <h3 className="text-lg sm:text-xl font-bold mb-2 text-white">{project.title}</h3>
+                            {/* <p className="text-gray-300 mb-4 flex-grow">{project.description}</p> */}
+                            <div className="flex flex-col sm:flex-row sm:justify-between gap-4">
                                 <div className="mb-4 text-left">
                                     {Object.entries(project.tools).map(([category, tools], idx) => (
                                         <div key={idx} className="mb-2">
@@ -30,7 +45,7 @@ export default function MultiCardCarousel({ projects }) {
                                         </div>
                                     ))}
                                 </div>
-                                <img loading="lazy" src={project.image} alt={project.title} className=" max-w-3/5 h-auto aspect-auto"/>
+                                <img loading="lazy" src={project.image} alt={project.title} className="max-w-full sm:max-w-3/5 h-auto object-contain mx-auto sm:mx-0 rounded"/>
                             </div>
 
                             <a href={project.link} target="_blank" rel="noopener noreferrer" className="flex shrink-0 justify-center mt-4 mx-auto">
