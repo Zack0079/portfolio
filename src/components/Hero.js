68cefea88ee17549d9fcb1293e96edd463e6c1cb
Zack0079 <zackmoonca@gmail.com>
2026-09-24 17:28:34 -0400

    Updated Hero.js reponsive and fix the bug that scroll down text & icon_bk that may double section height

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -25,80 +25,74 @@ export default function Hero() {
   }, []);
 
   return (
-    <section id="hero" className="bg-gray-900 text-white h-screen">
+    <section id="hero" className="relative bg-gray-900 text-white min-h-dvh w-screen flex flex-col px-4 sm:px-6 md:px-8 pt-12 lg:pt-0">
 
 
-      <div className=" flex flex-col items-center justify-center w-full h-full mx-auto text-center">
-        <h1 className="text-8xl font-bold mb-4 bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-5xl font-extrabold text-transparent ">Zack Tse</h1>
-        {/* <p className="text-lg mb-8 ">I am Full-Stack Developer and DevOps. Have 5 yaers of experience.<br/>
+      <div id="hero-content" className="relative z-10 flex-1 flex flex-col justify-center items-center text-center">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold mb-4 bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text font-extrabold text-transparent">Zack Tse</h1>
+          <p className="text-base sm:text-lg text-gray-300">
+            {/* I am Full-Stack Developer and DevOps. Have 5 yaers of experience.<br/>
             I have developed different applications software with different tectnologies.<br/>
             Also, I love learn new technologies and skills and share with others.<br/>
-            Please feel free to reach me. </p> */}
-        <p className="text-lg text-gray-300">
+            Please feel free to reach me.*/}
 
-          Passionate about learning new technologies and skills<br />
-        </p>
+            Passionate about learning new technologies and skills<br />
+          </p>
 
-        <div className="mt-8">
-          <h2 className="text-3xl font-bold mb-4">Full-Stack Developer</h2>
-          <div className="text-gray-300 flex flex-row justify-center space-x-4 mb-4">
-            {
-              full_stack_tech && (
-                full_stack_tech.map((tech, index) => (
-                  <button key={index} className="md:text-xl md:font-bold rounded-full outline-2 px-3 py-1  hover:text-white">{tech}</button>
-                ))
-              )}
+          <div className="mt-8 w-full">
+            <h2 className="text-2xl sm:text-3xl font-bold mb-4">Full-Stack Developer</h2>
+            <div className="text-gray-300 flex flex-row flex-wrap justify-center gap-3 mb-4">
+              {
+                full_stack_tech && (
+                  full_stack_tech.map((tech, index) => (
+                    <button key={index} className="text-sm sm:text-base md:text-xl md:font-bold rounded-full outline-2 px-3 py-1  hover:text-white">{tech}</button>
+                  ))
+                )}
+            </div>
+          </div>
+
+
+          <div className="mt-8 w-full">
+            <h2 className="text-2xl sm:text-3xl font-bold mb-4">Game Developer</h2>
+            <div className="text-gray-300 flex flex-row flex-wrap justify-center gap-3 mb-4">
+              {
+                game_dev_tech && (
+                  game_dev_tech.map((tech, index) => (
+                    <button key={index} className="text-sm sm:text-base md:text-xl md:font-bold rounded-full outline-2 px-3 py-1">{tech}</button>
+                  ))
+                )}
+            </div>
+          </div>
+
+          <div className="text-base md:text-xl text-gray-100 mt-8 flex flex-col md:flex-row flex-wrap justify-center gap-3 mb-4">
+            <a href="#projects" className="rounded-full bg-gray-600 px-3 py-2 text-gray-300  hover:text-white">
+              <span>View My Work</span>
+            </a>
+
+            <a href="#contact" className="rounded-full bg-gray-600 px-3 py-2 text-gray-300  hover:text-white">
+              <span>Contact</span>
+            </a>
+
+            <a href="https://github.com/Zack0079" target="_blank" className="rounded-full bg-gray-600 px-3 py-2 text-gray-300  hover:text-white">
+              <span>GitHub</span>
+            </a>
           </div>
         </div>
 
 
-        <div className="mt-8">
-          <h2 className="text-3xl font-bold mb-4">Game Developer</h2>
-          <div className="text-gray-300 flex flex-row justify-center space-x-4 mb-4">
-            {
-              game_dev_tech && (
-                game_dev_tech.map((tech, index) => (
-                  <button key={index} className="md:text-xl md:font-bold rounded-full outline-2 px-3 py-1">{tech}</button>
-                ))
-              )}
-          </div>
-        </div>
-
-
-        <div className="md:text-xl  text-gray-100 mt-8 flex flex-row justify-center space-x-4 mb-4">
-          <a href="#projects" className="rounded-full bg-gray-600 px-3 py-2 text-gray-300  hover:text-white">
-            <span>View My Work</span>
-          </a>
-
-          
-          
-          <a href="#contact" className="rounded-full bg-gray-600 px-3 py-2 text-gray-300  hover:text-white">
-            <span>Contact</span>
-          </a>
-
-          <a href="https://github.com/Zack0079" target="_blank" className="rounded-full bg-gray-600 px-3 py-2 text-gray-300  hover:text-white">
-            <span>GitHub</span>
-          </a>
-        </div>
-
-        <div className="absolute text-gray-300 bottom-10 flex flex-col text-xl items-center animate-bounce">
+        <div className="relative z-10 w-full text-center items-center animate-bounce text-gray-300 py-4 lg:py-8 text-lg sm:text-xl">
           <span className="text-gray-300 bottom-0">Scroll Down</span>
           <FontAwesomeIcon icon={faArrowDown} />
         </div>
 
       </div>
 
-      <div id="icon_bk" className="z-0 top-0 left-0 w-dvh h-dvh hidden xl:block">
+      <div id="icon_bk" className="absolute inset-0 z-0 w-full h-full hidden xl:block">
         {icon_bk.map((icon, index) => (
           <img key={index} src={`/assets/icons/${icon}.png`} alt="background icon" className={`absolute float-bob ${icon_pos[index]} size-15`} />
         ))}
       </div>
-      {/* <a
-          href="#contact"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Contact Me
-        </a> */}
     </section>
   );
 }
