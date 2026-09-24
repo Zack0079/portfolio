b5740cc16572613fc2e0e2744275aa9836a846a6
Zack0079 <zackmoonca@gmail.com>
2026-09-24 17:54:56 -0400

    Fixed the z-index too high that interrupt overlap.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -28,7 +28,7 @@ export default function Hero() {
     <section id="hero" className="relative bg-gray-900 text-white min-h-dvh w-screen flex flex-col px-4 sm:px-6 md:px-8 pt-12 lg:pt-0">
 
 
-      <div id="hero-content" className="relative z-10 flex-1 flex flex-col justify-center items-center text-center">
+      <div id="hero-content" className="relative z-1 flex-1 flex flex-col justify-center items-center text-center">
         <div className="flex flex-col justify-center items-center">
           <h1 className="text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold mb-4 bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text font-extrabold text-transparent">Zack Tse</h1>
           <p className="text-base sm:text-lg text-gray-300">
@@ -65,7 +65,7 @@ export default function Hero() {
             </div>
           </div>
 
-          <div className="text-base md:text-xl text-gray-100 mt-8 flex flex-col md:flex-row flex-wrap justify-center gap-3 mb-4">
+          <div className="z-1 text-base md:text-xl text-gray-100 mt-8 flex flex-col md:flex-row flex-wrap justify-center gap-3 mb-4">
             <a href="#projects" className="rounded-full bg-gray-600 px-3 py-2 text-gray-300  hover:text-white">
               <span>View My Work</span>
             </a>
@@ -81,7 +81,7 @@ export default function Hero() {
         </div>
 
 
-        <div className="relative z-10 w-full text-center items-center animate-bounce text-gray-300 py-4 lg:py-8 text-lg sm:text-xl">
+        <div className="relative z-1 w-full text-center items-center animate-bounce text-gray-300 py-4 lg:py-8 text-lg sm:text-xl">
           <span className="text-gray-300 bottom-0">Scroll Down</span>
           <FontAwesomeIcon icon={faArrowDown} />
         </div>
