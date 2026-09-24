5a4228ae844a10b99fa4a8e7288c505235758fa1
Zack0079 <zackmoonca@gmail.com>
2026-09-24 17:32:12 -0400

    Modified footer to be responsive in mobile size

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-gray-800 text-white py-4 text-center">
-      <div className="container mx-auto flex flex-row gap-4 justify-between">
+      <div className="container mx-auto flex flex-col sm:flex-row gap-2 sm:gap-4 justify-between items-center text-center sm:text-left px-4 text-sm sm:text-base">
         <span className="">&copy; {new Date().getFullYear()} Cheung Yuet Tse (Zack Tse). All rights reserved.</span >
         <span className="">Built with Next.js, FontAwesome, and Tailwind CSS</span>
       </div>
